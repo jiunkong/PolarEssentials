e4036bd46da4fff0d952b460e196ea10c558f4f0
Bukgeuk_ <BukgeukTV@gmail.com>
2021-01-02 15:04:47 +0900

    Update setspawn command

--- src/main/kotlin/command/CommandSpawn.kt
+++ src/main/kotlin/command/CommandSpawn.kt
@@ -16,11 +16,11 @@ import kotlin.math.roundToLong
 
 data class World(
     val location: List<Double>, // [x, y, z]
-    val direction: List<Float> // [yaw, pitch]
+    val direction: List<Float>, // [yaw, pitch]
+    val world: String
 )
 
 class CommandSetSpawn(private val dataFolder: String): CommandExecutor {
-
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender is Player) {
             val data: MutableMap<String, World>
@@ -35,22 +35,43 @@ class CommandSetSpawn(private val dataFolder: String): CommandExecutor {
                 mutableMapOf()
             }
 
-            data[sender.world.name] = World(
-                    listOf(sender.location.x, sender.location.y, sender.location.z),
-                    listOf(sender.location.yaw, sender.location.pitch)
-            )
+            if (args.isNotEmpty()) {
+                if (data[args[0]] == null) {
+                    sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Error: ${ChatColor.RESET}알 수 없는 월드: ${args[0]}")
+                    return true
+                }
+                data[sender.world.name] = data[args[0]]!!
 
-            mapper.writerWithDefaultPrettyPrinter().writeValue(file, data)
+                mapper.writerWithDefaultPrettyPrinter().writeValue(file, data)
 
-            sender.sendMessage(
+                val location = data[args[0]]!!.location
+                sender.sendMessage(
                     "${Color.ORANGE}이제 ${Color.RED}${sender.world.name}${Color.ORANGE} 세계의 스폰은 " +
-                            "${Color.RED}${String.format("%.1f", sender.location.x)}, ${String.format("%.1f", sender.location.y)}, ${String.format("%.1f", sender.location.z)}${Color.ORANGE} 입니다"
-            )
+                            "${Color.RED}${location[0]}, ${location[1]}, ${location[2]}, ${data[args[0]]!!.world}${Color.ORANGE} 입니다"
+                )
+            } else {
+                data[sender.world.name] = World(
+                    listOf(sender.location.x, sender.location.y, sender.location.z),
+                    listOf(sender.location.yaw, sender.location.pitch),
+                    sender.world.name
+                )
 
+                mapper.writerWithDefaultPrettyPrinter().writeValue(file, data)
+
+                sender.sendMessage(
+                    "${Color.ORANGE}이제 ${Color.RED}${sender.world.name}${Color.ORANGE} 세계의 스폰은 " +
+                            "${Color.RED}${String.format("%.1f", sender.location.x)}, ${
+                                String.format(
+                                    "%.1f",
+                                    sender.location.y
+                                )
+                            }, ${String.format("%.1f", sender.location.z)}, ${sender.world.name}${Color.ORANGE} 입니다"
+                )
+            }
             return true
         }
 
-        sender.sendMessage("Error: This command isn't available on the console")
+        sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Error: ${ChatColor.RESET}This command isn't available on the console")
 
         return true
     }
@@ -83,12 +104,12 @@ class CommandSpawn(private val dataFolder: String): CommandExecutor {
             val direction = data[sender.world.name]!!.direction
 
             sender.sendMessage("${Color.RED}스폰${Color.ORANGE}으로 이동합니다")
-            sender.teleport(Location(sender.world, location[0], location[1], location[2], direction[0], direction[1]))
+            sender.teleport(Location(Bukkit.getWorld(data[sender.world.name]!!.world), location[0], location[1], location[2], direction[0], direction[1]))
 
             return true
         }
 
-        sender.sendMessage("Error: This command isn't available on the console")
+        sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Error: ${ChatColor.RESET}This command isn't available on the console")
 
         return true
     }
